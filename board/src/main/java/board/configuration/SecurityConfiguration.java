fccab79f15ff11f625a084592d1bf4ecab58cf8a
JDEV <qkrwoghd04@naver.com>
2025-02-06 15:31:13 +0900

    feat: jwt 토근 구현 (아직 완전하게 작동하지 않음)

--- board/src/main/java/board/configuration/SecurityConfiguration.java
+++ board/src/main/java/board/configuration/SecurityConfiguration.java
@@ -5,17 +5,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import board.security.CustomAuthenticationSuccessHandler;
+import board.security.JwtRequestFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
     @Autowired
     private CustomAuthenticationSuccessHandler successHandler;
-
+    
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+    
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
@@ -27,12 +33,22 @@ public class SecurityConfiguration {
             .loginPage("/login")
             .loginProcessingUrl("/loginProc")
             .permitAll()
-            // .defaultSuccessUrl("/board")
             .successHandler(successHandler)
         );
         
-        // 개발단계에서 임시적으로 Disable 
         http.csrf(auth -> auth.disable());
+        
+        http.sessionManagement(auth -> auth
+            .sessionFixation(ses -> ses.newSession())
+            .maximumSessions(1)
+            .maxSessionsPreventsLogin(true)
+        );
+        
+//        http.sessionManagement(auth -> auth.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        http.logout(auth -> auth.logoutUrl("/logout").logoutSuccessUrl("/"));
+        
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         
         return http.build();
     }
@@ -42,3 +58,4 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 }
+
--- board/src/main/java/board/security/CustomAuthenticationSuccessHandler.java
+++ board/src/main/java/board/security/CustomAuthenticationSuccessHandler.java
@@ -1,23 +1,35 @@
 package board.security;
 
 import java.io.IOException;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import board.common.JwtUtils;
 import board.entity.UserEntity;
 import board.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private JwtUtils jwtUtils;
     
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -25,8 +37,12 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         UserEntity userEntity = userRepository.findByUsername(userDetails.getUsername());
         
-        request.getSession().setAttribute("user", userEntity);
+        String jwtToken = jwtUtils.generateToken(userEntity);
+        response.setHeader("token", jwtToken);
+                
         
-        response.sendRedirect("/");
+        // request.getSession().setAttribute("user", userEntity);
+        
+//        response.sendRedirect("/");
     }
 }
